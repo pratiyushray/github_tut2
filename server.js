9bc8e90e7ccbb3e19f24651174ef8ec595328173
Pratiyush Kumar Ray <64453528+pratiyushray@users.noreply.github.com>
2025-08-19 23:45:17 +0530

    Fix/abcdxyz (#4)
    * jwt first code
    
    * second jwt commit

--- server.js
+++ server.js
@@ -13,17 +13,13 @@ app.listen(port, () => {
 
 console.log("THIS IS SERVER JS")
 
+jwt.sign({
+  data: 'foobar'
+}, 'secret', { expiresIn: 60 * 60 });
 
+//or even better:
+
+jwt.sign({
+  data: 'foobar'
+}, 'secret', { expiresIn: '1h' });
 console.log("THIS IS ANOTHER CHANGE AFTER TWO PR MERGE")
-
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
