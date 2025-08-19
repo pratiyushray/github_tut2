44d0a15d80f7328f857a966aaff09d105009900c
Pratiyush Kumar Ray <64453528+pratiyushray@users.noreply.github.com>
2025-08-19 23:35:41 +0530

    Feat/xyzabcd (#3)
    * this is express code 1
    
    * this is second express code

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
 console.log("THIS IS SERVER JS")
 
 
 console.log("THIS IS ANOTHER CHANGE AFTER TWO PR MERGE")
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
