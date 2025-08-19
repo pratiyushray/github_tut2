e1046ff4f3322a0615f991073bb17b9921e7596e
Pratiyush Ray <pkray0021@gmail.com>
2025-08-19 23:33:33 +0530

    second jwt commit

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 console.log("THIS IS SERVER JS")
 
+jwt.sign({
+  data: 'foobar'
+}, 'secret', { expiresIn: 60 * 60 });
 
+//or even better:
+
+jwt.sign({
+  data: 'foobar'
+}, 'secret', { expiresIn: '1h' });
 console.log("THIS IS ANOTHER CHANGE AFTER TWO PR MERGE")
 
 jwt.sign({
