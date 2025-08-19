87316c71850fce044ace82946ae5d04bd896c49d
Pratiyush Ray <pkray0021@gmail.com>
2025-08-19 23:32:08 +0530

    jwt first code

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ console.log("THIS IS SERVER JS")
 
 
 console.log("THIS IS ANOTHER CHANGE AFTER TWO PR MERGE")
+
+jwt.sign({
+  data: 'foobar'
+}, 'secret', { expiresIn: 60 * 60 });
+
+//or even better:
+
+jwt.sign({
+  data: 'foobar'
+}, 'secret', { expiresIn: '1h' });
