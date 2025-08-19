4e0489790dfdd48b0b975c55fbdca1f164d2a8c3
Pratiyush Ray <pkray0021@gmail.com>
2025-08-19 23:30:14 +0530

    this is express code 1

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ console.log("THIS IS SERVER JS")
 
 
 console.log("THIS IS ANOTHER CHANGE AFTER TWO PR MERGE")
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
